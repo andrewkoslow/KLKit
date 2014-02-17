6f613d49ddf06d0331047e67445802887d567474
Andrew Koslow <andrewkoslow@gmail.com>
2014-02-17 21:34:41 +0400

    Kit importing header added

---
+++ KLKit/KLKit.h
@@ -0,0 +1,86 @@
+//
+//  KLKit.h
+//  KLKit
+//
+//  Created by Andrew Koslow on 17.02.14.
+//  Copyright (c) 2014 Andrew Koslow. All rights reserved.
+//
+
+
+#pragma mark - KLCasting
+
+#import"NSArray+KLCasting.h"
+#import"NSDictionary+KLCasting.h"
+#import"NSObject+KLCasting.h"
+#import"NSOrderedSet+KLCasting.h"
+#import"NSSet+KLCasting.h"
+
+
+#pragma mark - KLCollections
+
+#import"NSArray+KLCollections.h"
+#import"NSDictionary+KLCollections.h"
+#import"NSOrderedSet+KLCollections.h"
+#import"NSSet+KLCollections.h"
+
+
+#pragma mark - KLDataManipulation
+
+#import"NSData+KLDataManipulation.h"
+#import"NSString+KLDataManipulation.h"
+
+
+#pragma mark - KLDescription
+
+#import"NSArray+KLDescription.h"
+#import"NSDictionary+KLDescription.h"
+#import"NSOrderedSet+KLDescription.h"
+#import"NSSet+KLDescription.h"
+
+
+#pragma mark - KLExceptions
+
+#import"KLExceptions.h"
+
+
+#pragma mark - KLFileSystem
+
+#import"NSFileManager+KLFileSystem.h"
+
+
+#pragma mark - KLGraphics
+
+#import"KLGeometryFunctions.h"
+#import"UINib+KLGraphics.h"
+#import"UIView+KLGraphics.h"
+
+
+#pragma mark - KLKVO
+
+#import"NSObject+KLKVO.h"
+
+
+#pragma mark - KLLog
+
+#import"KLLog.h"
+
+
+#pragma mark - KLNotifications
+
+#import"NSObject+KLNotifications.h"
+
+
+#pragma mark - KLRuntime
+
+#import"KLRuntime.h"
+
+
+#pragma mark - KLSystemInfo
+
+#import"UIDevice+KLSystemInfo.h"
+
+
+#pragma mark - KLUserData
+
+#import"NSUserDefaults+KLUserData.h"
+
